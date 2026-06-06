8cac0c342a3252bf5cb068a7c16fa2135fe74207
Alraies97 <haedrtheking24@gmail.com>
2026-06-06 10:03:45 +0300

    fix a ValueError

--- app/core/security.py
+++ app/core/security.py
@@ -1,8 +1,7 @@
 from datetime import datetime, timedelta, timezone
-from passlib.context import CryptContext
 import jwt
+import bcrypt
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = "SUPER_SECRET_FOR_NOW"
 ALGORITHM = "HS256"
@@ -10,13 +9,21 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def hash_password(password: str) -> str:
     password_bytes = password.encode("utf-8")[:72]
-    return pwd_context.hash(password_bytes)
+    
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password_bytes,salt)
 
+    return hashed.decode("utf-8")   
 
-def verify_password(password: str, hashed_password: str) -> bool:
-    plain_password_bytes = plain_password.encode("utf-8")[:72]
-    return pwd_context.verify(plain_password_bytes, hashed_password)
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    try:
+        plain_password_bytes = plain_password.encode("utf-8")
+        hashed_password_bytes = hashed_password.encode("utf-8")
 
+        return bcrypt.checkpw(plain_password_bytes, hashed_password_bytes)
+    except:
+        return False
+    
 
 def create_token(data: dict) -> str:
     to_encode = data.copy()
